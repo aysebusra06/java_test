9d55822a82286253984e84f384a7792947508356
aysebusra06 <av.ayseinceoglu@gmail.com>
2023-01-31 19:47:03 +0300

    first commit

---
+++ C01_programlamayaGiris.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class C01_programlamayaGiris {
+    public static void main(String[] args) {
+
+                System.out.println("Hello world");
+                // println  yazdirdiktan sonra alt satira gecer
+                System.out.print("Bugun kodlamada ilk gunumuz");
+        /*
+        sadece print yazarsak yazdirdiktan sonra alt satira gecmez
+        bir sonraki yazdirdigimiz ayni satirdan devam eder
+        */
+    }
+}
